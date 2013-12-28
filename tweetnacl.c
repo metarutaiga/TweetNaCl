beb6e9d10092d39ce9ec220d76b53c87abff049e
TweetNaCl <@>
2013-12-29 00:00:00 +0800

    TweetNaCl version 20131229: tweaked for portability to platforms with 16-bit int.

--- tweetnacl.c
+++ tweetnacl.c
@@ -3,7 +3,7 @@
 #define sv static void
 
 typedef unsigned char u8;
-typedef unsigned int u32;
+typedef unsigned long u32;
 typedef unsigned long long u64;
 typedef long long i64;
 typedef i64 gf[16];
