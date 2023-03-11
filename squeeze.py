4b21b8e7fef0a813511a74ce28bc6e07f8ab3cf1
TAiGA <>
2023-03-11 23:24:29 +0800

    Support chacha20

--- squeeze.py
+++ squeeze.py
@@ -8,22 +8,22 @@ while True:
   if not line: break
   if line[0] == '#':
     if output:
-      print output
+      print(output)
       output = ''
-    print line.strip()
+    print(line.strip())
   else:
     x = re.findall('\w+|\W',line)
     for u in x:
       if not u.isspace():
         if len(output) + len(u) > 140:
-          print output
-	  output = ''
+          print(output)
+          output = ''
         if (re.match('\w',output[-1:]) and re.match('\w',u[:1])) or (output[-1:] == '=' and u[:1] == '-'):
           if len(output) + 1 + len(u) > 140:
-	    print output
-	    output = ''
-	  else:
+            print(output)
+            output = ''
+          else:
             output += ' '
         output += u
 
-print output
+print(output)
--- tweetnacl.h.py
+++ tweetnacl.h.py
@@ -1,17 +1,17 @@
-print '#ifndef TWEETNACL_H'
-print '#define TWEETNACL_H'
+print('#ifndef TWEETNACL_H')
+print('#define TWEETNACL_H')
 
 for z in [
 'box:curve25519xsalsa20poly1305/32/32/32/24/32/16:PUBLICKEYBYTES,SECRETKEYBYTES,BEFORENMBYTES,NONCEBYTES,ZEROBYTES,BOXZEROBYTES:'
 + ',_open,_keypair,_beforenm,_afternm,_open_afternm:qpuppp,qpuppp,qq,qpp,qpupp,qpupp',
-'core:salsa20/64/16/32/16,hsalsa20/32/16/32/16:OUTPUTBYTES,INPUTBYTES,KEYBYTES,CONSTBYTES::qppp',
+'core:salsa20/64/16/32/16,hsalsa20/32/16/32/16,chacha20/64/16/32/16:OUTPUTBYTES,INPUTBYTES,KEYBYTES,CONSTBYTES::qppp',
 'hashblocks:sha512/64/128:STATEBYTES,BLOCKBYTES::qpu',
 'hash:sha512/64:BYTES::qpu',
 'onetimeauth:poly1305/16/32:BYTES,KEYBYTES:,_verify:qpup,ppup',
 'scalarmult:curve25519/32/32:BYTES,SCALARBYTES:,_base:qpp,qp',
 'secretbox:xsalsa20poly1305/32/24/32/16:KEYBYTES,NONCEBYTES,ZEROBYTES,BOXZEROBYTES:,_open:qpupp,qpupp',
 'sign:ed25519/64/32/64:BYTES,PUBLICKEYBYTES,SECRETKEYBYTES:,_open,_keypair:qvpup,qvpup,qq',
-'stream:xsalsa20/32/24,salsa20/32/8:KEYBYTES,NONCEBYTES:,_xor:qupp,qpupp',
+'stream:xsalsa20/32/24,salsa20/32/8,chacha20/32/8:KEYBYTES,NONCEBYTES:,_xor,_ietf_xor:qupp,qpupp,qpupup',
 'verify:16/16,32/32:BYTES::pp'
 ]:
   x,q,s,f,g = [i.split(',') for i in z.split(':')]
@@ -22,15 +22,15 @@ for z in [
     op = o+'_'+p[0]
     opi = op+'_'+'tweet'
     if sel:
-      print '#define '+o+'_PRIMITIVE "'+p[0]+'"'
-      for m in f+['_'+m for m in s+['IMPLEMENTATION','VERSION']]: print '#define '+o+m+' '+op+m
+      print('#define '+o+'_PRIMITIVE "'+p[0]+'"')
+      for m in f+['_'+m for m in s+['IMPLEMENTATION','VERSION']]: print('#define '+o+m+' '+op+m)
       sel = 0
-    for j in range(len(s)): print '#define '+opi+'_'+s[j]+' '+str(p[j+1])
+    for j in range(len(s)): print('#define '+opi+'_'+s[j]+' '+str(p[j+1]))
     for j in range(len(f)):
       a = g[j].replace('v','u *').replace('u',',unsigned long long').replace('q',',unsigned char *').replace('p',',const unsigned char *')
-      print 'extern int '+opi+f[j]+'('+a[1:]+');'
-    print '#define '+opi+'_VERSION "-"'
-    for m in f+['_'+m for m in s+['VERSION']]: print '#define '+op+m+' '+opi+m
-    print '#define '+op+'_IMPLEMENTATION "'+o+'/'+p[0]+'/tweet'+'"'
+      print('extern int '+opi+f[j]+'('+a[1:]+');')
+    print('#define '+opi+'_VERSION "-"')
+    for m in f+['_'+m for m in s+['VERSION']]: print('#define '+op+m+' '+opi+m)
+    print('#define '+op+'_IMPLEMENTATION "'+o+'/'+p[0]+'/tweet'+'"')
 
-print '#endif'
+print('#endif')
